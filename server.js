f8af8b8eb32aed80c40cd7587c1b59e0de48ff78
abdulladone94 <abdulladone94@gmail.com>
2022-01-19 21:54:28 +0530

    MongoDb connection

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 require("dotenv").config();
 const exprss = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = exprss();
-const PORT = 3002;
+
+const PORT = process.env.PORT || 8070;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Mongodb Connection Success!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port: ${PORT}`);
